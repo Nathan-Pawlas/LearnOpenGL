766cf7e7c490edce3c12856c136f9c3947a90b70
BuzJr <112666026+BuzJr@users.noreply.github.com>
2023-08-08 20:12:50 -0400

    Everything for a HELLO TRIANGLE, with NOTES!

--- src/Source.cpp
+++ src/Source.cpp
@@ -3,11 +3,29 @@
 #include <iostream>
 #include <stdio.h>
 
+//VERTEX SHADER
+const char* vertexShaderSource = "#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//FRAGMENT SHADER
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 int main()
 {
+	// ----------- INITIALIZATION AND WINDOW -----------------
 	//Initialize and Configure GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -37,15 +55,94 @@ int main()
 		return -1;
 	}
 
-	//RENDER LOOP
+
+	// ------------- SHADER PROGRAMS ------------
+	//CREATE VERTEX SHADER
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); //CREATE THE SHADER
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //ATTACH SHADER SOURCE CODE TO SHADER OBJECT
+	glCompileShader(vertexShader);
+			//CHECK FOR COMPILE ERRORS
+			int success;
+			char infoLog[512];
+			glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+				std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+				return -1;
+			}
+
+	//CREATE FRAGMENT SHADER
+	unsigned int fragShader;
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER); //CREATE THE SHADER
+	glShaderSource(fragShader, 1, &fragmentShaderSource, NULL); //ATTACH SHADER SOURCE CODE TO SHADER OBJECT
+	glCompileShader(fragShader);
+			//CHECK FOR COMPILE ERRORS
+			glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+				std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+				return -1;
+			}
+
+	//CREATE SHADER PROGRAM
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader); //LINK VERTEX SHADER
+	glAttachShader(shaderProgram, fragShader); //LINK FRAG SHADER
+	glLinkProgram(shaderProgram);
+			//CHECK FOR LINK ERRORS
+			glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+				std::cout << "ERROR::SHADER::PROGRAM::LINKAGE_FAILED\n" << infoLog << std::endl;
+				return -1;
+			}
+	glDeleteShader(vertexShader); //DELETE VERTEX SHADER
+	glDeleteShader(fragShader); // DELETE FRAG SHADER
+
+
+	// ----------- VERTEX BUFFER & VERTEX ARRAY ----------------
+	//VERTEX DATA
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+	unsigned int VBO;
+	unsigned int VAO;
+	glGenBuffers(1, &VBO); //CREATE AN OPENGL VERTEX BUFFER OBJECT
+	glGenVertexArrays(1, &VAO); //CREATE A VERTEX ARRAY OBJECT
+
+	glBindVertexArray(VAO); //BIND VERTEX ARRAY (BEFORE BINDING VERT BUFFER!)
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //SPECIFY OBJECT TYPE (Vertex Buffer)
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //COPY VERTEX DATA TO VBO
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //SPECIFY TO OPENGL HOW TO INTERPRET VERTEX DATA
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //WE CAN UNBIND THE VBO B/C THE VBO IS REGISTERED WITH THE VAO NOW
+	glBindVertexArray(0); //UNBIND THE VAO SO OTHER VAO CALLS WON'T EFFECT IT (We'll rebind it when drawing)
+
+
+	// --------- RENDER LOOP --------------
 	while (!glfwWindowShouldClose(window))
 	{
 		//INPUT
 		processInput(window);
 
 		//RENDERING
-		glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
+		glClearColor(0.5f, 0.8f, 1.0f, 1.0f); //SET WINDOW COLOR
 		glClear(GL_COLOR_BUFFER_BIT);
+		//DRAW
+		glUseProgram(shaderProgram); //SPECIFY SHADERS TO USE
+		glBindVertexArray(VAO); //BIND VAO (HAS PTR TO VBO DATA IN IT)
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//POLL EVENTS, SWAP BUFFERS
 		glfwPollEvents();
